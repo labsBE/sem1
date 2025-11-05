9a7416229c49ad7189eb101e42a44d43ec93689b
labsBE <mayurc_c22080@students.isquareit.edu.in>
2025-11-05 23:00:27 +0530

    Update uber.py

--- ML/uber.py
+++ ML/uber.py
@@ -37,7 +37,7 @@ def haversine_distance(lat1, lon1, lat2, lon2):
     dphi = np.radians(lat2 - lat1)
     dlambda = np.radians(lon2 - lon1)
 
-    a = np.sin(dphi/2)*2 + np.cos(phi1) * np.cos(phi2) * np.sin(dlambda/2)*2
+    a = np.sin(dphi/2)**2 + np.cos(phi1) * np.cos(phi2) * np.sin(dlambda/2)**2
     c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
     
     return r * c
